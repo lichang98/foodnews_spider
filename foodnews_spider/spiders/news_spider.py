466d0f863cb5c318e24a9870ca59369b6d805d3c
lichang98 <15365811995@163.com>
2019-02-02 23:27:45 +0800

    已完成环球网的爬虫部分

--- foodnews_spider/spiders/news_spider.py
+++ foodnews_spider/spiders/news_spider.py
@@ -10,8 +10,8 @@ class NewsSpider(scrapy.Spider):
     allowed_domains = ['cctv.com', 'cntv.cn']
     # start_urls = ['http://news.cntv.cn/20110601/102300.shtml']
     # TODO start_url测试使用新闻门户搜索结果页面，使用多层爬取
-    start_urls = ['https://search.cctv.com/search.php?'
-                  'qtext=%E9%A3%9F%E5%93%81%E5%AE%89%E5%85%A8%E4%BA%8B%E4%BB%B6&type=web']
+    start_urls = [
+        'https://search.cctv.com/search.php?qtext=%E9%A3%9F%E5%93%81%E5%AE%89%E5%85%A8%E4%BA%8B%E4%BB%B6&type=web']
 
     def parse(self, response):
         """
@@ -23,17 +23,17 @@ class NewsSpider(scrapy.Spider):
         pageLinkList = response.xpath('//div[@class="tuwenjg"]//a[@id and @href]/@id').extract()
         pageLinkUrlList = response.xpath('//div[@class="tuwenjg"]//a[@id and @href]/@href').extract()
         # 页面链接的值需要提取处理
-        pageLinkUrlList = [link[link.find('http'):link.find('html')+4] for link in pageLinkUrlList]
+        pageLinkUrlList = [link[link.find('http'):link.find('html') + 4] for link in pageLinkUrlList]
         for i in range(len(pageLinkList)):
             if str(pageLinkList[i]).startswith('web_content'):
-                yield scrapy.Request(pageLinkUrlList[i], meta={'url': pageLinkUrlList[i]}, callback=self.page_parse)
+                yield scrapy.Request(pageLinkUrlList[i], meta={'url': pageLinkUrlList[i]}, callback=self.cctv_page_parse)
         next_page = response.xpath('//a[@class="page-next"]').extract()
         if next_page:  # 如果搜索结果有下一页
             url = response.xpath('//a[@class="page-next"]//@href').extract()[0]
-            url = "{}://{}/{}".format('http',response.url.split('/')[2],url)
-            print('debug: nextpageurl = ',url)
+            url = "{}://{}/{}".format('http', response.url.split('/')[2], url)
+            print('debug: nextpageurl = ', url)
             # 处理完毕当前页面的搜索结果，迭代处理下一个搜索页面
-            yield scrapy.Request(url,callback=self.parse)
+            yield scrapy.Request(url, callback=self.parse)
 
         return None
         # ld = ItemLoader(item=FoodnewsSpiderItem(), response=response)
@@ -44,18 +44,55 @@ class NewsSpider(scrapy.Spider):
         # ld.add_xpath('content', '//div[@class="body"]/p/text()')
         # return ld.load_item()
 
-    def page_parse(self, response):
+    def cctv_page_parse(self, response):
         """
         页面内容解析,第二层
         :param response:
         :return:
         """
-        print('debug: 第二层解析器获得url: ',response.url)
-        ld=ItemLoader(item=FoodnewsSpiderItem(),response=response)
-        ld.add_value('url',response.meta['url'])
+        print('debug: 第二层解析器获得url: ', response.url)
+        ld = ItemLoader(item=FoodnewsSpiderItem(), response=response)
+        ld.add_value('url', response.meta['url'])
         ld.add_xpath('title', '//h1[@class="b-tit"][1]/text()')
         ld.add_xpath('title', '//div[@class="cnt_bd"]/h1[1]/text()')
         ld.add_xpath('content', '//div[@class="cnt_bd"]/p/text()')
         ld.add_xpath('content', '//div[@class="body"]/p/text()')
         return ld.load_item()
 
+
+class HuanqiuNewsSpider(scrapy.Spider):
+    name="huanqiu_spider"
+    allowed_domains=['huanqiu.com']
+    start_urls=['http://health.huanqiu.com/food_safety/']
+    prev_url='' # 用于记录上一层解析的网页url
+
+    def parse(self, response):
+        """
+        第一层析取
+        :param response:
+        :return:
+        """
+        pageLinks=response.xpath('//li[@class="item"]//a/@href').extract()
+        pageLinks=list(set(pageLinks))
+        for link in pageLinks:
+            yield scrapy.Request(link,callback=self.page_parse)
+        # 获取下一个第一层页面链接
+        self.prev_url=response.url
+        next_url=response.xpath('//a[@class="a1"][last()]/@href').extract()[0]
+        if next_url and self.prev_url != next_url:
+            yield scrapy.Request(next_url,callback=self.parse)
+        return None
+
+    def page_parse(self, response):
+        """
+        第二层析取，爬取新闻页面内容
+        :param response:
+        :return:
+        """
+        ld=ItemLoader(item=FoodnewsSpiderItem(),response=response)
+        ld.add_value('url',response.url)
+        ld.add_xpath('title','//h1[@class="tle"][1]/text()')
+        ld.add_xpath('title','//div[@class="conText"]/h1[1]/text()')
+        ld.add_xpath('content','//div[@class="la_con"]//p/text()')
+        ld.add_xpath('content','//div[@class="conText"]//p/text()')
+        return ld.load_item()
